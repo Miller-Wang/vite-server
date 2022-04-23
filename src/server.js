08456e77bcf9e722f821b3ac11fb1447f57de7e7
Miller <millera@163.com>
2022-04-23 15:19:23 +0800

    5.实现vue文件解析

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const Koa = require('koa');
 const serveStaticPlugin = require('./serveStaticPlugin');
 const moduleRewritePlugin = require('./moduleRewritePlugin');
 const moduleResolvePlugin = require('./moduleResolvePlugin');
+const vuePlugin = require('./vuePlugin');
 
 function createServer() {
   const app = new Koa();
@@ -9,7 +10,7 @@ function createServer() {
   const context = { app, root };
 
   // 内部使用app.use注册中间件
-  [moduleRewritePlugin, moduleResolvePlugin, serveStaticPlugin].forEach((plugin) => plugin(context));
+  [moduleRewritePlugin, moduleResolvePlugin, vuePlugin, serveStaticPlugin].forEach((plugin) => plugin(context));
 
   return app;
 }
---
+++ src/vuePlugin.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const { parse, compileScript, compileTemplate, rewriteDefault } = require('@vue/compiler-sfc');
+const fs = require('fs').promises;
+
+const descriptorCache = new Map();
+
+function vuePlugin({ root, app }) {
+  app.use(async (ctx, next) => {
+    if (!ctx.path.endsWith('.vue')) {
+      return await next();
+    }
+    // 处理vue文件
+    const filePath = path.join(root, ctx.path);
+    const { descriptor } = await getDescriptor(filePath);
+
+    // 组装代码
+    let targetCode = '';
+
+    // 处理脚本
+    if (descriptor.script) {
+      let scriptCode = compileScript(descriptor, { reactivityTransform: false });
+      scriptCode = rewriteDefault(scriptCode.content, '_sfc_main');
+      targetCode += scriptCode;
+    }
+
+    // 处理模版
+    if (descriptor.template) {
+      let { code } = compileTemplate({ source: descriptor.template.content });
+      targetCode += code;
+    }
+
+    targetCode += `\n_sfc_main.render = render;\nexport default _sfc_main;`;
+    ctx.type = 'js';
+    ctx.body = targetCode;
+  });
+}
+
+async function getDescriptor(filePath) {
+  if (descriptorCache.has(filePath)) {
+    return descriptorCache.get(filePath);
+  }
+
+  const content = await fs.readFile(filePath, 'utf8');
+  const descriptor = parse(content);
+  descriptorCache.set(filePath, descriptor);
+  return descriptor;
+}
+
+module.exports = vuePlugin;
