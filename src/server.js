971ef541246aaddd42a21566c9124a153f1fb447
Miller <millera@163.com>
2022-04-23 13:16:58 +0800

    2.静态文件服务

--- src/server.js
+++ src/server.js
@@ -1,1 +1,16 @@
-console.log('vite-server');
+const Koa = require('koa');
+const static = require('koa-static');
+
+function createServer() {
+  const app = new Koa();
+  const root = process.cwd();
+  const context = { app, root };
+  // 1.静态文件服务
+  app.use(static(root));
+
+  return app;
+}
+
+createServer().listen(4000, async () => {
+  console.log(`vite running at: http://localhost:4000`);
+});
